f827dfc433c48e00ee6d433c6f1e4328ec7dc4d6
Hugo-Andres-Gaspar <handresgaspar@gmail.com>
2024-03-29 16:36:17 +0100

    Actualización Main

--- CentroEducativo/src/instituto/ProgramaInstituto.java
+++ CentroEducativo/src/instituto/ProgramaInstituto.java
@@ -5,26 +5,30 @@ import java.util.ArrayList;
 public class ProgramaInstituto {
 
 	public static void main(String[] args) {
-
+		// Crear arrayList de personas
 		ArrayList<Persona> personas = new ArrayList<>();
 		
+		// Agregar trabajadores
 		Director director = new Director("Juan", "García", "12345678A", 45, "Masculino", 50000.0, "Dirección", "Director", 20, "Licenciado en Educación");
 		Profesor profesor = new Profesor("Ana", "Martínez", "98765432B", 35, "Femenino", 30000.0, "Educación", "Profesor", "Matemáticas", "Licenciada en Matemáticas", 40);
 		personas.add(director);
 		personas.add(profesor);
 		
+		// Agregar alumnos
 		Alumno alumno1 = new Alumno("Pedro", "López", "11111111X", 18, "Masculino", 12, "Secundaria", true);
 		Alumno alumno2 = new Alumno("María", "Sánchez", "22222222Y", 17, "Femenino", 11, "Secundaria", false);
 		personas.add(alumno1);
 		personas.add(alumno2);
 		
 		// Mostrar los datos de todas las personas de la lista
+		System.out.println("Detalles de las personas: ");
 		for (Persona persona : personas) {
 			persona.imprimirPersona();
 		}
 		
 		
 		// Mostrar el estodo de todos los alumnos
+		System.out.println("Estado de los alumnos: ");
 		for (Persona persona : personas) {
 			if (persona instanceof Alumno) {
 				Alumno alumno = (Alumno) persona;
@@ -34,6 +38,7 @@ public class ProgramaInstituto {
 		
 		
 		// Mostrar presentacion de todas las personas
+		System.out.println("Presentación de las personas: ");
 		for (Persona persona : personas) {
 			persona.presentarse();
 		}
